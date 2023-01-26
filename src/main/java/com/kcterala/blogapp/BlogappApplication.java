65dc4a90d4f3c4efb20dec7f7a6f128edb443778
kc_terala <kcterala@gmail.com>
2023-01-26 16:58:58 +0530

    Git Testing

--- src/main/java/com/kcterala/blogapp/BlogappApplication.java
+++ src/main/java/com/kcterala/blogapp/BlogappApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BlogappApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hello world");
 		SpringApplication.run(BlogappApplication.class, args);
 	}
 
